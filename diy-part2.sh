9d735ccfd31ecae11a96cad37a4a3ecc676fa734
Filtration <aocial@gmail.com>
2021-11-01 21:47:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 
 sed -i 's/ntp.aliyun.com/time.pool.aliyun.com/g' package/base-files/files/bin/config_generate
 sed -i 's/time1.cloud.tencent.com/time.asia.apple.com/g' package/base-files/files/bin/config_generate
